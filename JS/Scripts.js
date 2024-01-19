0cd53328c10c4169ebc3c7a7e1a8e9c0427533b4
OmegaSatirus <cristianorodriguespimenta2405@gmail.com>
2024-01-19 11:56:59 -0300

    Inicio JS Estilização das Paginas

---
+++ JS/Scripts.js
@@ -0,0 +1,18 @@
+const Carrinho=[]
+const ListaDeProdutos = [
+    {Nome:"Comida01", Preco: 25.99},
+    {Nome:"Comida02", Preco: 12.95},
+    {Nome:"Comida03", Preco: 6.70},
+    {Nome:"Comida04", Preco: 14.90},
+    {Nome:"Comida05", Preco: 34.99},
+    {Nome:"Comida06", Preco: 23.70},
+    {Nome:"Comida07", Preco: 49.99},
+    {Nome:"Comida08", Preco: 25.99},
+    {Nome:"Comida09", Preco: 24.99},
+    {Nome:"Comida10", Preco: 23.99},
+    {Nome:"Comida11", Preco: 19.99},
+    {Nome:"Comida12", Preco: 25.99},
+]
+fuction AdcProduto(){
+    document.getElementById
+}
